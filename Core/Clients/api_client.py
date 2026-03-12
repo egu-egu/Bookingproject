067a0dd7983acb613bb68742e10c9810a9400c04
Egor <egu-egu@mail.ru>
2026-03-12 08:37:27 +0300

    Вынес создание бронирования в отдельный файл, в апи клиенте добавил в существующие заголовки

--- Core/Clients/api_client.py
+++ Core/Clients/api_client.py
@@ -87,9 +87,8 @@ class APIClient:
     def create_booking(self, booking_data):
         with allure.step('Creating booking'):
             url = f"{self.base_url}{Endpoints.BOOKING_ENDPOINT.value}"
-            headers = {
-                'Content-Type': 'application/json'
-            }
+            headers = self.session.headers.copy()
+            headers.pop('Authorization', None)
             response = requests.post(url, json=booking_data, headers=headers)
             response.raise_for_status()
         with allure.step('Checking status code'):
--- Core/tests/test_health_check.py
+++ Core/tests/test_health_check.py
@@ -58,33 +58,7 @@ def test_ping_timeout(api_client, mocker):
     with pytest.raises(requests.Timeout):
         api_client.ping()
 
-@allure.feature('Test Booking')
-@allure.story('Test Created booking')
-def test_create_booking(api_client, mocker):
-    payload = {
-    "firstname" : "Jim",
-    "lastname" : "Brown",
-    "totalprice" : 111,
-    "depositpaid" : True,
-    "bookingdates" : {
-        "checkin" : "2018-01-01",
-        "checkout" : "2019-01-01"
-    },
-    "additionalneeds" : "Breakfast"
-}
-    response = api_client.create_booking(payload)
-    assert "bookingid" in response
-    assert "booking" in response
 
-
-    booking = response["booking"]
-    assert booking["firstname"] == "Jim"
-    assert booking["lastname"] == "Brown"
-    assert booking["totalprice"] == 111
-    assert booking["depositpaid"] == True
-    assert booking["bookingdates"]["checkin"] == "2018-01-01"
-    assert booking["bookingdates"]["checkout"] == "2019-01-01"
-    assert booking["additionalneeds"] == "Breakfast"
 
 
 
